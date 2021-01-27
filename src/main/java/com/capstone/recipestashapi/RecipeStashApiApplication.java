8878e1cddff5ac7d35f1ec06b27c907f8ed7e09e
Jasmine <jasylop@gmail.com>
2021-01-27 12:54:58 -0800

    initialized spring boot set up

---
+++ src/main/java/com/capstone/recipestashapi/RecipeStashApiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.recipestashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeStashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeStashApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/recipestashapi/RecipeStashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.recipestashapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeStashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
