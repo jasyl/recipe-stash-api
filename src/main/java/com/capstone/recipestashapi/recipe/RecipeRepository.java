e837f66fa79f5b681337a21eaab2427e47ef1f07
Jasmine <jasylop@gmail.com>
2021-01-27 17:50:49 -0800

    created RecipeRepository

--- src/main/java/com/capstone/recipestashapi/recipe/RecipeRepository.java
+++ src/main/java/com/capstone/recipestashapi/recipe/RecipeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository  // data access
-public interface StudentRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
 }
