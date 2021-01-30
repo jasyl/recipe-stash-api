a4db17cf4fbcc6838b7d2266576dab0572475aab
Jasmine <jasylop@gmail.com>
2021-01-29 19:48:00 -0800

    added some notes

--- src/main/java/com/capstone/recipestashapi/recipe/RecipeRepository.java
+++ src/main/java/com/capstone/recipestashapi/recipe/RecipeRepository.java
@@ -3,7 +3,9 @@ package com.capstone.recipestashapi.recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository  // data access
+@Repository  // data access object DAO
+// this class allows us to call methods like findbyID -> refer to the JpaRepostiory library
+// https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
 }
