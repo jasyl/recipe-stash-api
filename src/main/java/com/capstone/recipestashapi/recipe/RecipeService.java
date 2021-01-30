1ca2efdb0d1ee52a35da32022133f229ae175fdf
Jasmine <jasylop@gmail.com>
2021-01-29 19:48:23 -0800

    updated get recipes method

--- src/main/java/com/capstone/recipestashapi/recipe/RecipeService.java
+++ src/main/java/com/capstone/recipestashapi/recipe/RecipeService.java
@@ -9,13 +9,20 @@ import java.util.List;
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public RecipeService(RecipeRepository recipeRepository) {
+    public RecipeService(RecipeRepository recipeRepository, UserRepository userRepository) {
         this.recipeRepository = recipeRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<Recipe> getRecipes() {
-        return recipeRepository.findAll();
+    public List<Recipe> getRecipes(long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalStateException("user with id " + userId + " does not exist"));
+        return user.getRecipes();
+    }
+
+    public void addRecipe(Recipe recipe) {
+        recipeRepository.save(recipe);
     }
 }
