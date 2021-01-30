a8ec578f120d55a78addb4d461868d3d050d82ba
Jasmine <jasylop@gmail.com>
2021-01-29 19:45:11 -0800

    added rest template for external api calls

--- src/main/java/com/capstone/recipestashapi/RecipeStashApiApplication.java
+++ src/main/java/com/capstone/recipestashapi/RecipeStashApiApplication.java
@@ -1,10 +1,42 @@
 package com.capstone.recipestashapi;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 public class RecipeStashApiApplication {
+
+//	@Bean
+//	public WebClient getWebClient() {
+//
+//		return WebClient.builder()
+//				.baseUrl("https://api.spoonacular.com/recipes")
+//				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//				.build();
+//	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+//	@Bean(name="entityManagerFactory")
+//	public LocalSessionFactoryBean sessionFactory() {
+//		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//		return sessionFactory;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeStashApiApplication.class, args);
