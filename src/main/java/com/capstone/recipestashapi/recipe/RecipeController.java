51a93f5742891faedc37aad9d18c64764fcf71fc
Jasmine <jasylop@gmail.com>
2021-01-29 19:47:34 -0800

    updated get mapping

--- src/main/java/com/capstone/recipestashapi/recipe/RecipeController.java
+++ src/main/java/com/capstone/recipestashapi/recipe/RecipeController.java
@@ -1,25 +1,51 @@
 package com.capstone.recipestashapi.recipe;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/users/{userId}")
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public RecipeController(RecipeService recipeService) {
+    public RecipeController(RecipeService recipeService, RestTemplate restTemplate) {
         this.recipeService = recipeService;
+        this.restTemplate = restTemplate;
     }
 
-    @GetMapping
-    public List<Recipe> getRecipes() {
-        return recipeService.getRecipes();
+    @GetMapping(path = "/recipes", produces = "application/json")
+    public List<Recipe> getRecipes(@PathVariable long userId ) {
+        return recipeService.getRecipes(userId);
+    }
+
+    @GetMapping(path = "/recipes/{recipeId}", produces = "application/json")
+    public List<Recipe> getRecipes(@PathVariable long userId, @PathVariable long recipeId) {
+
+        return recipeService.getRecipes(userId);
+    }
+
+    @PostMapping(path = "/recipes", consumes = "application/json", produces = "application/json")
+    public void addRecipe(@PathVariable long userId, @RequestBody Recipe recipe) {
+        recipeService.addRecipe(recipe);
+    }
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @RequestMapping(path = "/{query}")
+    public void getRecipesExternal(@PathVariable long userId, @PathVariable String query) {
+
+        final String uri = "https://api.spoonacular.com/recipes/complexSearch" + "?apiKey=" + apiKey + "&query=" + query ;
+        String result = restTemplate.getForObject(uri, String.class);
+
+        System.out.println(result);
+
     }
 }
