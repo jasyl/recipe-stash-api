1a26c5e7c0d3e61ea780ecf5143033b42a2e6364
Jasmine <jasylop@gmail.com>
2021-01-30 12:08:15 -0800

    added an ingredients repo not sure it's necessary

--- src/main/java/com/capstone/recipestashapi/recipe/IngredientsRepository.java
+++ src/main/java/com/capstone/recipestashapi/recipe/IngredientsRepository.java
@@ -1,2 +1,8 @@
-package com.capstone.recipestashapi.recipe;public interface IngredientsRepository {
+package com.capstone.recipestashapi.recipe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientsRepository extends JpaRepository<Ingredient, Long> {
 }
