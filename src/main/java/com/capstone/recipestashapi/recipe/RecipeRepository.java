06fb4353060c62f6d977f6d6a3064042a9c51882
Jasmine <jasylop@gmail.com>
2021-01-30 16:29:35 -0800

    added a method to findrecipebysourceurl

--- src/main/java/com/capstone/recipestashapi/recipe/RecipeRepository.java
+++ src/main/java/com/capstone/recipestashapi/recipe/RecipeRepository.java
@@ -3,9 +3,11 @@ package com.capstone.recipestashapi.recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository  // data access object DAO
 // this class allows us to call methods like findbyID -> refer to the JpaRepostiory library
 // https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-
+    Optional<Recipe> findRecipeBySourceUrl(String url);
 }
